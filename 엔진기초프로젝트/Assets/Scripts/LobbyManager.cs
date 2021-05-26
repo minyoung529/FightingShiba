8aabee484bfc8d998bcbdce59340c2007a3f4bad
minyoung529 <77655318+minyoung529@users.noreply.github.com>
2021-05-27 08:30:40 +0900

    20210527 로비 버튼과 메인 정지 버튼 추가

--- 엔진기초프로젝트/Assets/Scripts/LobbyManager.cs
+++ 엔진기초프로젝트/Assets/Scripts/LobbyManager.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LobbyManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private GameObject QuitPopUp = null;
+
+    private void Update()
     {
-        
+        if(QuitPopUp.activeInHierarchy && Input.GetKeyDown(KeyCode.Escape))
+        {
+            QuitPopUp.SetActive(false);
+        }
+
+        else if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            QuitPopUp.SetActive(true);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnClickStart()
     {
-        
+        SceneManager.LoadScene(0);
+    }
+
+    public void OnClickCheckQuit()
+    {
+        QuitPopUp.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
